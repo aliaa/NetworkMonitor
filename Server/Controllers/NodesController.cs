e152ee889d4ada4b166cd36ddc30eddb590ea8b0
Ali Aboutalebi <aboutalebi@live.com>
2020-11-17 14:48:42 +0330

    nodes definition

---
+++ Server/Controllers/NodesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using NetworkMonitor.Server.Models;
+using NetworkMonitor.Shared.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NetworkMonitor.Server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class NodesController : BaseController
+    {
+        private readonly IMongoCollection<NetworkNode> nodesCol;
+
+        public NodesController(IMongoCollection<NetworkNode> nodesCol, IMongoCollection<AuthUserX> usersCol) : base(usersCol)
+        {
+            this.nodesCol = nodesCol;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(List<NetworkNode> nodes)
+        {
+            await nodesCol.InsertManyAsync(nodes);
+            return Ok();
+        }
+    }
+}
---
+++ Shared/Models/NetworkNode.cs
@@ -0,0 +1,22 @@
+﻿using EasyMongoNet;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetworkMonitor.Shared.Models
+{
+    public class NetworkNode : MongoEntity
+    {
+        public enum CheckMechanismEnum
+        {
+            Ping,
+            Http
+        }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        [Display(Name = "Checking Mechanism")]
+        public CheckMechanismEnum CheckMechanism { get; set; }
+    }
+}
